af3c710bdf4d3ff30912bdfbcade675f9006846a
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-10-21 18:57:09 +0500

    API applicant predict service 1.0.0

--- app/validates/user_validate.py
+++ app/validates/user_validate.py
@@ -11,43 +11,43 @@ from app.validates.valid_values import (
 
 class UserValidate(UserSchema):
     @field_validator("gender")
-    def validate_gender(self, value: str) -> str:
+    def validate_gender(self, value):
         if value not in VALID_GENDERS:
             raise ValueError(f"Gender must be {VALID_GENDERS}")
         return value
 
     @field_validator("needs_hostel")
-    def validate_needs_hostel(self, value: str) -> str:
+    def validate_needs_hostel(self, value):
         if value not in VALID_NEEDS_HOSTELS:
             raise ValueError(f"Needs hostel must be in {VALID_NEEDS_HOSTELS}")
         return value
 
     @field_validator("average_rate")
-    def validate_average_rate(self, value: float) -> float:
+    def validate_average_rate(self, value):
         if not (1 <= value <= 5):
             raise ValueError(f"Average rate must be in range(1, 5)")
         return value
 
     @field_validator("priority")
-    def validate_priority(self, value: int) -> int:
+    def validate_priority(self, value):
         if value not in range(2, 6):
             raise ValueError("Priority must be in range(1, 5)")
         return value
 
     @field_validator("total_exam_points")
-    def validate_total_exam_points(self, value: int) -> int:
+    def validate_total_exam_points(self, value):
         if value not in range(0, 310):
             raise ValueError("Total exam points must be in range(0, 300)")
         return value
 
     @field_validator("total_bonus_points")
-    def validate_total_bonus_points(self, value: int) -> int:
+    def validate_total_bonus_points(self, value):
         if value not in range(0, 11):
             raise ValueError("Total bonus points must be in range(0, 10)")
         return value
 
     @field_validator('education')
-    def validate_education(self, value) -> str:
+    def validate_education(self, value):
         if value not in VALID_EDUCATIONS:
             raise ValueError(f"Education must be in {VALID_EDUCATIONS}")
         return value
